30545376b4d9463bf080bbad6031f6abdbb2555f
Markus Deibel <github2018@markus-deibel.de>
2020-04-04 09:10:34 +0200

    Lots of fixes
    - Rendering
    - Buzzing
    - Button handling

--- WorkoutHeartRateMonitor.js
+++ WorkoutHeartRateMonitor.js
@@ -5,29 +5,77 @@ const Setter = {
     LOWER: 'lower'
 };
     
-const shortBuzzTimeInMs = 50;
-const longBuzzTimeInMs = 200;
+const shortBuzzTimeInMs = 80;
+const longBuzzTimeInMs = 400;
 
-let upperLimit = 90;
+let upperLimit = 130;
 let upperLimitChanged = true;
 
-let lowerLimit = 50;
+let lowerLimit = 100;
 let lowerLimitChanged = true;
 
 let limitSetter = Setter.NONE;
 
 let currentHeartRate = 0;
 let hrConfidence = -1;
-let hrOrConfidenceChanged = true;
-
+let hrChanged = true;
+let confidenceChanged = true;
 
 let setterHighlightTimeout;
+
+function renderUpperLimitBackground() {
+  g.setColor(1,0,0);
+  g.fillRect(125,40, 210, 70);
+  g.fillRect(180,70, 210, 200);
+
+  //Round top left corner
+  g.fillEllipse(115,40,135,70);
+
+  //Round top right corner
+  g.setColor(0,0,0);
+  g.fillRect(205,40, 210, 45);
+  g.setColor(1,0,0);
+  g.fillEllipse(190,40,210,50);
+
+  //Round inner corner
+  g.fillRect(174,71, 179, 76);
+  g.setColor(0,0,0);
+  g.fillEllipse(160,71,179,82);
+
+  //Round bottom
+  g.setColor(1,0,0);
+  g.fillEllipse(180,190, 210, 210);
+}
+
+function renderLowerLimitBackground() {
+  g.setColor(0,0,1);
+  g.fillRect(10, 180, 100, 210);
+  g.fillRect(10, 50, 40, 180);
+
+  //Rounded top
+  g.setColor(0,0,1);
+  g.fillEllipse(10,40, 40, 60);
+
+  //Round bottom right corner
+  g.setColor(0,0,1);
+  g.fillEllipse(90,180,110,210);
+
+  //Round inner corner
+  g.setColor(0,0,1);
+  g.fillRect(40,175,45,180);
+  g.setColor(0,0,0);
+  g.fillEllipse(41,170,60,179);
+
+  //Round bottom left corner
+  g.setColor(0,0,0);
+  g.fillRect(10,205, 15, 210);
+  g.setColor(0,0,1);
+  g.fillEllipse(10,200,30,210);
+}
 
 function drawTrainingHeartRate() {
   //Only redraw if the display is on
   if (Bangle.isLCDOn()) {
-    renderButtonIcons();
-  
     renderUpperLimit();
   
     renderCurrentHeartRate();
@@ -43,95 +91,56 @@ function drawTrainingHeartRate() {
 function renderUpperLimit() {
   if(!upperLimitChanged) { return; }
   
-  g.setColor(255,0,0);
+  g.setColor(1,0,0);
   g.fillRect(125,40, 210, 70);
-  g.fillRect(180,70, 210, 200);
-
-  //Round top left corner
-  g.fillEllipse(115,40,135,70);
-
-  //Round top right corner
-  g.setColor(0,0,0);
-  g.fillRect(205,40, 210, 45);
-  g.setColor(255,0,0);
-  g.fillEllipse(190,40,210,50);
-
-  //Round inner corner
-  g.fillRect(174,71, 179, 76);
-  g.setColor(0,0,0);
-  g.fillEllipse(160,71,179,82);
-
-  //Round bottom
-  g.setColor(255,0,0);
-  g.fillEllipse(180,190, 210, 210);
-
+  
   if(limitSetter === Setter.UPPER){
     g.setColor(255,255, 0);
   } else {
     g.setColor(255,255,255);
   }
-  g.setFontVector(10);
+  g.setFontVector(13);
   g.drawString("Upper  : " + upperLimit, 130,50);
   
   upperLimitChanged = false;
 }
   
 function renderCurrentHeartRate() {
-  if(!hrOrConfidenceChanged) { return; }
+  if(!hrChanged) { return; }
   
   g.setColor(255,255,255);
-  g.fillRect(45, 110, 165, 140);
-  g.setColor(0,0,0);
-  g.setFontVector(13);
+  g.fillRect(55, 110, 165, 150);
 
-  g.drawString("Current:" , 65,117);
+  g.setColor(0,0,0);
+  g.setFontVector(24);
   g.setFontAlign(1, -1, 0);
-  g.drawString(currentHeartRate, 155, 117);
+  g.drawString(currentHeartRate, 130, 117);
 
   //Reset alignment to defaults
   g.setFontAlign(-1, -1, 0);
+
+  hrChanged = false;
 }
   
 function renderLowerLimit() {
   if(!lowerLimitChanged) { return; }
   
-  g.setColor(0,0,255);
+  g.setColor(0,0,1);
   g.fillRect(10, 180, 100, 210);
-  g.fillRect(10, 50, 40, 180);
-
-  //Rounded top
-  g.setColor(0,0,255);
-  g.fillEllipse(10,40, 40, 60);
-
-  //Round bottom right corner
-  g.setColor(0,0,255);
-  g.fillEllipse(90,180,110,210);
-
-  //Round inner corner
-  g.setColor(0,0,255);
-  g.fillRect(40,175,45,180);
-  g.setColor(0,0,0);
-  g.fillEllipse(41,170,60,179);
-
-  //Round bottom left corner
-  g.setColor(0,0,0);
-  g.fillRect(10,205, 15, 210);
-  g.setColor(0,0,255);
-  g.fillEllipse(10,200,30,210);
-
+  
   if(limitSetter === Setter.LOWER){
     g.setColor(255,255, 0);
   } else {
     g.setColor(255,255,255);
   }
-  g.setFontVector(10);
+  g.setFontVector(13);
   g.drawString("Lower  : " + lowerLimit, 20,190);
   
   lowerLimitChanged = false;
 }
   
 function renderConfidenceBars(){
-  if(!hrOrConfidenceChanged) { return; }
+  if(!confidenceChanged) { return; }
   
   if(hrConfidence >= 85){
       g.setColor(0, 255, 0);
@@ -143,48 +152,57 @@ function renderConfidenceBars(){
       g.setColor(255, 255, 255);
   }
 
-  g.fillRect(45, 110, 55, 140);
-  g.fillRect(165, 110, 175, 140);
+  g.fillRect(45, 110, 55, 150);
+  g.fillRect(165, 110, 175, 150);
+
+  confidenceChanged = false;
 }
   
 function renderButtonIcons() {
   g.setColor(255,255,255);
   g.setFontVector(14);
 
-  // + for Btn1
+  //+ for Btn1
   g.drawString("+", 222,50);
 
-  // Home for Btn2
+  //Home for Btn2
   g.drawLine(220, 118, 227, 110);
   g.drawLine(227, 110, 234, 118);
 
   g.drawPoly([222,117,222,125,232,125,232,117], false);
   g.drawRect(226,120,229,125);
 
-  // - for Btn3
+  //- for Btn3
   g.drawString("-", 222,165);
 }
   
 function buzz()
 {
+  // Do not buzz if not confident
+  if(hrConfidence < 85) { return; }
+
   if(currentHeartRate > upperLimit)
   {
      Bangle.buzz(shortBuzzTimeInMs);
-     setTimeout(() => { Bangle.buzz(shortBuzzTimeInMs); }, shortBuzzTimeInMs);
-     setTimeout(() => { Bangle.buzz(shortBuzzTimeInMs); }, shortBuzzTimeInMs);
+     setTimeout(() => { Bangle.buzz(shortBuzzTimeInMs); }, shortBuzzTimeInMs * 2);
   }
 
   if(currentHeartRate < lowerLimit)
   {
      Bangle.buzz(longBuzzTimeInMs);
-     setTimeout(() => { Bangle.buzz(longBuzzTimeInMs); }, longBuzzTimeInMs);
   }
 }
   
 function onHrm(hrm){
-  hrOrConfidenceChanged = (currentHeartRate !== hrm.bpm || hrConfidence !== hrm.confidence);
-  currentHeartRate = hrm.bpm;
-  hrConfidence = hrm.confidence;
+  if(currentHeartRate !== hrm.bpm){
+    currentHeartRate = hrm.bpm;
+    hrChanged = true;
+  }
+
+  if(hrConfidence !== hrm.confidence) {
+    hrConfidence = hrm.confidence;
+    confidenceChanged = true;
+  }
 }
   
 function setLimitSetterToLower() {
@@ -275,7 +293,12 @@ Bangle.on('lcdPower', (on) => {
   g.clear();
   if (on) {
     Bangle.drawWidgets();
+    renderButtonIcons();
     // call your app function here
+    renderLowerLimitBackground();
+    renderUpperLimitBackground();
+    lowerLimitChanged = true;
+    upperLimitChanged = true;
     drawTrainingHeartRate();
   }
 });
@@ -283,20 +306,20 @@ Bangle.on('lcdPower', (on) => {
 Bangle.setHRMPower(1);
 Bangle.on('HRM', onHrm);
  
-// refesh every sec
-setInterval(drawTrainingHeartRate, 1000);
-  
+setWatch(incrementLimit, BTN1, {edge:"rising", debounce:50, repeat:true});
+setWatch(switchOffApp, BTN2, {edge:"rising", debounce:50, repeat:true});
+setWatch(decrementLimit, BTN3, {edge:"rising", debounce:50, repeat:true});
+setWatch(setLimitSetterToLower, BTN4, {edge:"rising", debounce:50, repeat:true});
+setWatch(setLimitSetterToUpper, BTN5, { edge: "rising", debounce: 50, repeat: true });
+
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
-drawTrainingHeartRate();
-  
-setWatch(switchOffApp, BTN2, {repeat:false,edge:"falling"});
-  
-setWatch(incrementLimit, BTN1, {edge:"rising", debounce:50, repeat:true});
-  
-setWatch(decrementLimit, BTN3, {edge:"rising", debounce:50, repeat:true});
-  
-setWatch(setLimitSetterToLower, BTN4, {edge:"rising", debounce:50, repeat:true});
-  
-setWatch(setLimitSetterToUpper, BTN5, { edge: "rising", debounce: 50, repeat: true });
+//drawTrainingHeartRate();
+
+renderButtonIcons();
+renderLowerLimitBackground();
+renderUpperLimitBackground();
+
+// refesh every sec
+setInterval(drawTrainingHeartRate, 1000);
